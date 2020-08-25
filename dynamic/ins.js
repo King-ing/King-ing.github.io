94e525ade22f34b68f5710884f1f41f9f4582c1b
林健 <896263973@qq.com>
2020-08-25 20:54:53 +0800

    Site updated: 2020-08-25 20:54:52

--- dynamic/ins.js
+++ dynamic/ins.js
@@ -117,8 +117,8 @@
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
           var minSrc = 'https://cdn.jsdelivr.net/gh/King-ing/blog-Picture/min_photos/' + data.link[i];
-          // var minSrc = 'https://cdn.jsdelivr.net/gh/King-ing/CDN/assets/comment_bg-1.gif'
-          var src = 'https://cdn.jsdelivr.net/gh/King-ing/blog-Picture/photos/' + data.link[i];
+          // var minSrc = 'https://cdn.jsdelivr.net/gh/King-ing/CDN/assets/comment_bg-3.gif'
+          var src = 'https://cdn.jsdelivr.net/gh/King-ing/blog-Picture/Original_image/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
